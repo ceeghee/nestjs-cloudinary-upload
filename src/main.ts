1e6f545a0c39b0e9da98c461be047989ba6dccf0
Goodluck Ekene <goodluck.ekene@gmail.com>
2020-11-24 00:26:44 +0100

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 import { AppModule } from './app.module';
+const corsOptions = {
+	origin: '*',
+	preflightContinue: false,
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors(corsOptions)
 	const port: number = parseInt(process.env.PORT, 10) || 3000;
 	await app.listen(port);
 }
