a235b63a950f690bda98dcf7a78ed538b85b96a6
Goodluck Ekene <goodluck.ekene@gmail.com>
2020-11-24 00:20:43 +0100

    random changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(5000);
+	const port: number = parseInt(process.env.PORT, 10) || 3000;
+	await app.listen(port);
 }
 bootstrap();
