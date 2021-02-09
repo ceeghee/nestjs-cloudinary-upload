2cfeee6b1954f81c3514eb16b10969e91babeee5
Goodluck Ekene <goodluck.ekene@gmail.com>
2021-02-10 00:21:03 +0100

    fix bugs

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -8,9 +8,10 @@ import { v2 } from 'cloudinary';
 export class UploadService {
   constructor() {
     v2.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dgkacpt2u',
+      api_key: process.env.CLOUDINARY_API_KEY || '581928764961869',
+      api_secret:
+        process.env.CLOUDINARY_API_SECRET || '3cbRkTtuY9rZqJVH9kl9S3kZlrw',
     });
   }
 
