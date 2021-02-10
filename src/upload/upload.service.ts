c3c7c14082e1203020ab153ba2b0cf2eed4effbf
Goodluck Ekene <goodluck.ekene@gmail.com>
2021-02-10 02:11:57 +0100

    fix bugs

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -8,17 +8,14 @@ import { v2 } from 'cloudinary';
 export class UploadService {
   constructor() {
     v2.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dgkacpt2u',
-      api_key: process.env.CLOUDINARY_API_KEY || '581928764961869',
-      api_secret:
-        process.env.CLOUDINARY_API_SECRET || '3cbRkTtuY9rZqJVH9kl9S3kZlrw',
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
     });
   }
 
   async updateProfilePicture(props: any) {
-    console.log(props);
-
-    const { createReadStream, filename } = await props.file;
+    const { createReadStream } = await props.file;
     const result = await this.uploadStream(createReadStream);
     return result.secure_url;
   }
